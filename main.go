c6d828a8a110dc0a4b135e25bb5896df08133539
David González <nodearth@gmail.com>
2017-12-18 10:26:08 +0000

    added HalfClosed logic

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ func main() {
 	vishu := New(func(stats map[string]interface{}) int {
 		if stats["timeout"] != nil {
 			return stats["timeout"].(int)
-		} else {
-			return 0
 		}
+		return 0
 	})
 	vishu.Add("test")
 	vishu.Add("test2")
 	for i := 0; i < 10; i++ {
-		vishu.Choose(func(endpoint interface{}) (map[string]interface{}, error) {
+		vishu.With(func(endpoint interface{}) (map[string]interface{}, error) {
 			stats := make(map[string]interface{})
 			endpointStr := endpoint.(string)
 			if endpointStr == "test2" {
--- vishu.go
+++ vishu.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"errors"
+	"math"
 	"time"
 )
 
 type circuitStatus int
 
 const (
-	Open           = iota
-	Closed         = iota
-	HalfOpen       = iota
+	// Open circuit status.
+	Open = iota
+	// Closed circuit status.
+	Closed = iota
+	// HalfOpen circuitStatus
+	HalfOpen = iota
+	// ClosingTimeout is the default HalfOpen to Closed timeout.
 	ClosingTimeout = time.Second * 30
 )
 
@@ -21,6 +26,7 @@ type endpoint struct {
 	circuitStatus circuitStatus
 }
 
+// Vishu instance. It is the core of the module.
 type Vishu struct {
 	endpoints []endpoint
 	rater     rater
@@ -28,8 +34,17 @@ type Vishu struct {
 
 type rater func(map[string]interface{}) int
 
+func defaultRater(stats map[string]interface{}) int {
+	execTime := stats["__execution_time"]
+	if execTime != nil {
+		return math.MaxInt32 - int(execTime.(time.Duration))
+	}
+	return 0
+}
+
 type action func(interface{}) (map[string]interface{}, error)
 
+// New creates a Vishu instance with a custom rating function.
 func New(rater rater) *Vishu {
 	// TODO David: Needs default rater and default action
 	vishu := Vishu{nil, rater}
@@ -44,6 +59,7 @@ func newEndpoint(target interface{}) (*endpoint, error) {
 	return &endpoint{target, make(map[string]interface{}), 500, Closed}, nil
 }
 
+// Add adds an endpoint
 func (v *Vishu) Add(target interface{}) error {
 	endpoint, err := newEndpoint(target)
 	if err != nil {
@@ -53,27 +69,39 @@ func (v *Vishu) Add(target interface{}) error {
 	return nil
 }
 
-func (v *Vishu) Choose(action action) {
+// With selects an endpoint
+func (v *Vishu) With(action action) {
 	var max, index int
 	for i, element := range v.endpoints {
-		if element.score > max {
+		if element.circuitStatus != Open && element.score > max {
 			max = element.score
 			index = i
 		}
 	}
 
 	chosenEndpoint := &v.endpoints[index]
+
+	// Measure the execution time as a default metric
+	start := time.Now()
 	stats, error := action(v.endpoints[index].Target)
+	finish := time.Now()
+	elapsed := finish.Sub(start)
+	stats["__execution_time"] = elapsed
 
 	if error != nil {
-		chosenEndpoint.score = 500
+		chosenEndpoint.score = 0
 		chosenEndpoint.circuitStatus = Open
 
 		time.AfterFunc(ClosingTimeout, func() {
 			chosenEndpoint.circuitStatus = HalfOpen
 		})
+	} else {
+		if chosenEndpoint.circuitStatus == HalfOpen {
+			chosenEndpoint.score = 500
+			chosenEndpoint.circuitStatus = Closed
+		}
+		// If there is an error, no rating happens.
+		chosenEndpoint.score = v.rater(stats)
+		chosenEndpoint.stats = stats
 	}
-
-	chosenEndpoint.score = v.rater(stats)
-	chosenEndpoint.stats = stats
 }
